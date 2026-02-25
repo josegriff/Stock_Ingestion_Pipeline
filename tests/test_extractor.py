7996ce78772202b2c1c7831ae5a1ecf2d905c0f9
Jose Griffiths <79573468+josegriff@users.noreply.github.com>
2026-02-25 09:54:46 +0000

    Deletion of redundant folders

--- tests/test_extractor.py
+++
@@ -1,8 +0,0 @@
-import pytest
-from src.extractor import fetch_stock_data
-
-def test_fetch_stock_data():
-    data = fetch_stock_data("BP.L")
-    
-    assert isinstance(data, dict)           # Ensure returned data is a dictionary 
-    assert "Time Series (Daily)" in data    # Basic check for expected key  
